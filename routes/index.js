c89e81a5fd31c5ec7d946d460a43903686008c92
Akhil K <akhilkponnanivj45@gmail.com>
2021-12-31 13:06:12 +0530

    Updated Index home

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title:'Home'});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home', index: true });
 });
 
 module.exports = router;
